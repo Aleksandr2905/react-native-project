c2dfaaefa5749ec774b36b896ded2dd1e3ff7746
Aleksandr2905 <natalukha@ukr.net>
2024-02-13 16:39:34 +0200

    added component CommentsScreen

--- src/Screens/CommentsScreen.jsx
+++ src/Screens/CommentsScreen.jsx
@@ -1,38 +1,81 @@
-import { FlatList, Image, SafeAreaView, StyleSheet, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Comment } from "../components/Comment";
 import { comments } from "../data/comments";
 import photo from "../../assets/images/sea.jpg";
+import { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
 
 export const CommentsScreen = () => {
+  const [comment, setComment] = useState("");
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.wrapper}>
-        <Image source={photo} style={styles.photo} />
-        <FlatList
-          data={comments}
-          renderItem={({ item }) => <Comment item={item} />}
-          keyExtractor={(item) => item.id}
-          showsVerticalScrollIndicator={false}
+    <View style={styles.container}>
+      <FlatList
+        ListHeaderComponent={<Image source={photo} style={styles.photo} />}
+        data={comments}
+        renderItem={({ item }) => <Comment item={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+      />
+      <View>
+        <TextInput
+          name="comment"
+          placeholder="Коментувати..."
+          placeholderTextColor="#bdbdbd"
+          value={comment}
+          onChangeText={setComment}
+          style={styles.input}
         />
+        <TouchableOpacity style={styles.button}>
+          <AntDesign name="arrowup" size={14} color={"#fff"} />
+        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // width: "100%",
+    // height: "100%",
     paddingHorizontal: 16,
-    paddingTop: 32,
     backgroundColor: "#fff",
-  },
-  wrapper: {
-    width: "100%",
-    height: "100%",
   },
   photo: {
     width: "100%",
     height: 240,
     borderRadius: 8,
+    marginVertical: 32,
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderRadius: 100,
+    padding: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: "#e8e8e8",
+    fontSize: 16,
+    backgroundColor: "#f6f6f6",
+  },
+  button: {
+    position: "absolute",
+    width: 34,
+    height: 34,
+    top: 8,
+    right: 8,
+    borderRadius: 50,
+    backgroundColor: "#FF6C00",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,21 +1,64 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 
 export const Comment = ({ item }) => {
-  const { photo, comment, date } = item;
+  const { photo, comment, date, userId } = item;
   return (
-    <View style={styles.container}>
-      <Image source={photo} />
-      <Text>{comment}</Text>
-      <Text>{date}</Text>
+    <View style={[styles.container, userId === "current" && styles.current]}>
+      <Image source={photo} style={styles.photo} />
+      <View
+        style={[
+          styles.wrapperComment,
+          userId === "current"
+            ? { borderTopRightRadius: 0 }
+            : { borderTopLeftRadius: 0 },
+        ]}
+      >
+        <Text style={styles.comment}>{comment}</Text>
+        <Text
+          style={[
+            styles.date,
+            userId !== "current" && { alignSelf: "flex-end" },
+          ]}
+        >
+          {date}
+        </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    // backgroundColor: "#fff",
+    flex: 1,
+    flexDirection: "row",
     paddingBottom: 24,
-    // paddingHorizontal: 16,
     gap: 16,
+  },
+  current: {
+    flexDirection: "row-reverse",
+  },
+  photo: {
+    borderRadius: 50,
+    width: 28,
+    height: 28,
+  },
+  wrapperComment: {
+    flex: 1,
+    width: "100%",
+    padding: 16,
+    borderRadius: 6,
+    gap: 8,
+    backgroundColor: "rgba(0, 0, 0, 0.03)",
+  },
+  comment: {
+    width: "100%",
+    fontFamily: "Roboto-Regular",
+    fontSize: 13,
+    lineHeight: 18,
+    color: "#212121",
+  },
+  date: {
+    fontSize: 10,
+    color: "#bdbdbd",
   },
 });
